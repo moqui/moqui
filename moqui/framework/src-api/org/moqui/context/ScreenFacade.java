5e9b555d893841c25d9a332b34699bac749bfe8d
jonesde <jonesde@77d6ed94-2e2c-4b3b-a835-e9bc6a43c7e3>
2010-03-25 02:44:26 +0000

    A few changes: made screen rendering for different types of text more flexible so you can easily add your own types, plus added config for various types and conf options for the screen facade like the other tools; the SubscreensItem entity now has a userId in the PK so that subscreens menu settings can be configured per-user like many other things, also added _NA_ userId to apply it to all users; various cleanups including some missing entity relationships, and also updated notes; renamed MoquiConf.xml files to have unique names according to their use
    git-svn-id: https://moqui.svn.sourceforge.net/svnroot/moqui/trunk@12 77d6ed94-2e2c-4b3b-a835-e9bc6a43c7e3

--- moqui/framework/src-api/org/moqui/context/ScreenFacade.java
+++ moqui/framework/src-api/org/moqui/context/ScreenFacade.java
@@ -15,19 +15,24 @@
  */
 package org.moqui.context;
 
+import java.nio.charset.Charset;
+
 /** For rendering screens for general use (mostly for things other than web pages or web page snippets). */
 public interface ScreenFacade {
-    /** The output types available when rendering a screen as text. */
-    enum ScreenTextOutputType { HTML, XSL_FO, XML }
-
     /** Render a screen to an appender using the current context. The screen will run in a sub-context so the original
      * context will not be changed.
      *
      * @param screenLocation Location of the XML Screen file to render.
      * @param appender Textual output of the rendering will be appended to this.
      * @param outputType The type of output. Used to select sub-elements of the <code>platform-specific</code> element.
-     *                   If macroTemplateLocation is not specified is also used to determine the default macro template.
-     * @param macroTemplateLocation Location of an FTL file with macros used to generate output.
+     *                   If macroTemplateLocation is not specified is also used to determine the default macro template
+     *                   based on configuration. Can be anything. Default supported values include: text, html, xsl-fo,
+     *                   xml, and csv.
+     * @param characterEncoding The MIME character encoding for the text produced. Defaults to <code>UTF-8</code>. Must
+     *                          be a valid charset in the java.nio.charset.Charset class.
+     * @param macroTemplateLocation Location of an FTL file with macros used to generate output. If not specified macro
+     *                              file from the screen configuration will be used depending on the outputType.
      */
-    void renderScreenText(String screenLocation, Appendable appender, ScreenTextOutputType outputType, String macroTemplateLocation);
+    void renderScreenText(String screenLocation, Appendable appender, String outputType, String characterEncoding,
+                          String macroTemplateLocation);
 }
