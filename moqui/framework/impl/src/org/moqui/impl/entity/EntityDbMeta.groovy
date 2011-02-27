ec007172e3bd7be33825adaff611228d0d17b773
jonesde <jonesde@77d6ed94-2e2c-4b3b-a835-e9bc6a43c7e3>
2011-02-27 12:13:22 +0000

    Now checks for missing columns at runtime if a table does exist (ie wasn't just added) and adds them if missing
    git-svn-id: https://moqui.svn.sourceforge.net/svnroot/moqui/trunk@225 77d6ed94-2e2c-4b3b-a835-e9bc6a43c7e3

--- moqui/framework/impl/src/org/moqui/impl/entity/EntityDbMeta.groovy
+++ moqui/framework/impl/src/org/moqui/impl/entity/EntityDbMeta.groovy
@@ -19,15 +19,19 @@ import java.sql.ResultSet
 
 import org.moqui.entity.EntityException
 import javax.transaction.Transaction
+import org.moqui.context.Cache
+import java.sql.Timestamp
+import org.apache.commons.collections.set.ListOrderedSet
 
 class EntityDbMeta {
     protected final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(EntityDefinition.class)
 
-    protected Set<String> existingTableEntityNames = new HashSet<String>()
+    protected Cache entityTablesChecked
 
     protected EntityFacadeImpl efi
     EntityDbMeta(EntityFacadeImpl efi) {
         this.efi = efi
+        entityTablesChecked = efi.ecfi.cacheFacade.getCache("entity.tables.checked")
     }
 
     void checkTableRuntime(EntityDefinition ed) {
@@ -36,19 +40,55 @@ class EntityDbMeta {
         if (!addMissingRuntime) return
 
         // if it's in this table we've already checked it
-        if (existingTableEntityNames.contains(ed.entityName)) return
+        if (entityTablesChecked.containsKey(ed.entityName)) return
 
+        long startTime = System.currentTimeMillis()
         Transaction parentTransaction = null
         try {
             if (efi.ecfi.transactionFacade.isTransactionInPlace()) {
                 parentTransaction = efi.ecfi.transactionFacade.suspend()
             }
             // transaction out of the way, check/create
-            if (!tableExists(ed)) createTable(ed)
-        } finally {
-            if (parentTransaction != null) {
-                efi.ecfi.transactionFacade.resume(parentTransaction)
+            if (!tableExists(ed)) {
+                createTable(ed)
+                // TODO create indexes
+                // TODO create foreign keys to/from all other tables that exist
+            } else {
+                // table exists, see if it is missing any columns
+                ListOrderedSet mcs = getMissingColumns(ed)
+                if (mcs) for (String fieldName in mcs) {
+                    addColumn(ed, fieldName)
+                }
             }
+            entityTablesChecked.put(ed.entityName, new Timestamp(System.currentTimeMillis()))
+        } finally {
+            if (parentTransaction != null) efi.ecfi.transactionFacade.resume(parentTransaction)
+        }
+
+        logger.info("Checked table for entity [${ed.entityName}] in ${(System.currentTimeMillis()-startTime)/1000} seconds")
+    }
+
+    boolean tableExists(EntityDefinition ed) {
+        String groupName = efi.getEntityGroupName(ed.entityName)
+        Connection con = null
+        ResultSet tableSet = null
+        try {
+            con = efi.getConnection(groupName)
+            DatabaseMetaData dbData = con.getMetaData()
+
+            String[] types = ["TABLE", "VIEW", "ALIAS", "SYNONYM"]
+            tableSet = dbData.getTables(null, ed.getSchemaName(), ed.getTableName(), types)
+            if (tableSet.next()) {
+                return true
+            } else {
+                logger.info("Table for entity [${ed.entityName}] does NOT exist")
+                return false
+            }
+        } catch (Exception e) {
+            throw new EntityException("Exception checking to see if table [${ed.getTableName()}] exists", e)
+        } finally {
+            if (tableSet != null) tableSet.close()
+            if (con != null) con.close()
         }
     }
 
@@ -57,51 +97,34 @@ class EntityDbMeta {
         if (ed.isViewEntity()) throw new IllegalArgumentException("Cannot create table for a view entity")
 
         String groupName = efi.getEntityGroupName(ed.entityName)
-        Node databaseListNode = efi.ecfi.confXmlRoot."database-list"[0]
         Node databaseNode = efi.getDatabaseNode(groupName)
 
-        StringBuilder sql = new StringBuilder("CREATE TABLE ")
-        sql.append(ed.getFullTableName())
-        sql.append(" (")
+        StringBuilder sql = new StringBuilder("CREATE TABLE ").append(ed.getFullTableName()).append(" (")
 
         for (String fieldName in ed.getFieldNames(true, true)) {
             Node fieldNode = ed.getFieldNode(fieldName)
             String sqlType = efi.getFieldSqlType(fieldNode."@type", ed.entityName)
             String javaType = efi.getFieldJavaType(fieldNode."@type", ed.entityName)
 
-            sql.append(ed.getColumnName(fieldName, false))
-            sql.append(" ")
-            sql.append(sqlType)
+            sql.append(ed.getColumnName(fieldName, false)).append(" ").append(sqlType)
 
             if ("String" == javaType || "java.lang.String" == javaType) {
-                if (databaseNode."@character-set") {
-                    sql.append(" CHARACTER SET ")
-                    sql.append(databaseNode."@character-set")
-                }
-                if (databaseNode."@collate") {
-                    sql.append(" COLLATE ");
-                    sql.append(databaseNode."@collate");
-                }
+                if (databaseNode."@character-set") sql.append(" CHARACTER SET ").append(databaseNode."@character-set")
+                if (databaseNode."@collate") sql.append(" COLLATE ").append(databaseNode."@collate")
             }
 
             if (fieldNode."@is-pk" == "true") {
-                if (databaseNode."@always-use-constraint-keyword" == "true") {
-                    sql.append(" CONSTRAINT NOT NULL")
-                } else {
-                    sql.append(" NOT NULL")
-                }
+                if (databaseNode."@always-use-constraint-keyword" == "true") sql.append(" CONSTRAINT")
+                sql.append(" NOT NULL")
             }
             sql.append(", ")
         }
 
         if (databaseNode."@use-pk-constraint-names" != "false") {
             String pkName = "PK_" + ed.getTableName()
-
             int constraintNameClipLength = databaseNode."@constraint-name-clip-length" as int
             if (pkName.length() > constraintNameClipLength) pkName = pkName.substring(0, constraintNameClipLength)
-
-            sql.append("CONSTRAINT ")
-            sql.append(pkName)
+            sql.append("CONSTRAINT ").append(pkName)
         }
         sql.append(" PRIMARY KEY (")
         boolean isFirstPk = true
@@ -109,32 +132,79 @@ class EntityDbMeta {
             if (isFirstPk) isFirstPk = false else sql.append(", ")
             sql.append(ed.getColumnName(pkName, false))
         }
-        sql.append(")")
-
-        sql.append(")")
+        sql.append("))")
 
         // some MySQL-specific inconveniences...
-        if (databaseNode."@table-engine") {
-            sql.append(" ENGINE ")
-            sql.append(databaseNode."@table-engine")
-        }
-        if (databaseNode."@character-set") {
-            sql.append(" CHARACTER SET ")
-            sql.append(databaseNode."@character-set")
-        }
-        if (databaseNode."@collate") {
-            sql.append(" COLLATE ")
-            sql.append(databaseNode."@collate")
-        }
+        if (databaseNode."@table-engine") sql.append(" ENGINE ").append(databaseNode."@table-engine")
+        if (databaseNode."@character-set") sql.append(" CHARACTER SET ").append(databaseNode."@character-set")
+        if (databaseNode."@collate") sql.append(" COLLATE ").append(databaseNode."@collate")
 
         if (logger.traceEnabled) logger.trace("Create Table with SQL: " + sql.toString())
 
+        if (logger.infoEnabled) logger.info("Creating table [${ed.tableName}] for entity [${ed.entityName}]")
+        runSqlUpdate(sql, groupName)
+    }
+
+    ListOrderedSet getMissingColumns(EntityDefinition ed) {
+        String groupName = efi.getEntityGroupName(ed.entityName)
+        Connection con = null
+        ResultSet colSet = null
+        try {
+            con = efi.getConnection(groupName)
+            DatabaseMetaData dbData = con.getMetaData()
+
+            ListOrderedSet fnSet = ed.getFieldNames(true, true)
+            int fieldCount = fnSet.size()
+            colSet = dbData.getColumns(null, ed.getSchemaName(), ed.getTableName(), "%")
+            while (colSet.next()) {
+                String colName = colSet.getString("COLUMN_NAME")
+                String fieldName = null
+                for (String fn in fnSet) if (ed.getColumnName(fn, false) == colName) { fieldName = fn; break }
+                if (fieldName) fnSet.remove(fieldName)
+            }
+
+            if (fnSet.size() == fieldCount) {
+                logger.warn("Could not find any columns to match fields for entity [${ed.entityName}]")
+                return null
+            }
+            return fnSet
+        } catch (Exception e) {
+            throw new EntityException("Exception checking for missing columns in table [${ed.getTableName()}]", e)
+        } finally {
+            if (colSet != null) colSet.close()
+            if (con != null) con.close()
+        }
+    }
+
+    void addColumn(EntityDefinition ed, String fieldName) {
+        if (!ed) throw new IllegalArgumentException("No EntityDefinition specified, cannot create table")
+        if (ed.isViewEntity()) throw new IllegalArgumentException("Cannot create table for a view entity")
+
+        String groupName = efi.getEntityGroupName(ed.entityName)
+        Node databaseNode = efi.getDatabaseNode(groupName)
+
+        Node fieldNode = ed.getFieldNode(fieldName)
+        String sqlType = efi.getFieldSqlType(fieldNode."@type", ed.entityName)
+        String javaType = efi.getFieldJavaType(fieldNode."@type", ed.entityName)
+
+        StringBuilder sql = new StringBuilder("ALTER TABLE ").append(ed.getFullTableName())
+        // NOTE: if any databases need "ADD COLUMN" instead of just "ADD", change this to try both or based on config
+        sql.append(" ADD ").append(ed.getColumnName(fieldName, false)).append(" ").append(sqlType)
+
+        if ("String" == javaType || "java.lang.String" == javaType) {
+            if (databaseNode."@character-set") sql.append(" CHARACTER SET ").append(databaseNode."@character-set")
+            if (databaseNode."@collate") sql.append(" COLLATE ").append(databaseNode."@collate")
+        }
+
+        if (logger.infoEnabled) logger.info("Adding column [${ed.getColumnName(fieldName, false)}] to table [${ed.tableName}] for field [${fieldName}] of entity [${ed.entityName}]")
+        runSqlUpdate(sql, groupName)
+    }
+
+    void runSqlUpdate(StringBuilder sql, String groupName) throws EntityException {
         Connection con = null
         Statement stmt = null
         boolean beganTransaction = efi.ecfi.transactionFacade.begin(null)
         try {
-            if (logger.infoEnabled) logger.info("Creating table [${ed.tableName}] for entity [${ed.entityName}]")
-
             con = efi.getConnection(groupName)
             stmt = con.createStatement()
             stmt.executeUpdate(sql.toString())
@@ -148,33 +218,6 @@ class EntityDbMeta {
             if (efi.ecfi.transactionFacade.isTransactionInPlace()) {
                 efi.ecfi.transactionFacade.commit(beganTransaction)
             }
-        }
-    }
-
-    boolean tableExists(EntityDefinition ed) {
-        if (existingTableEntityNames.contains(ed.entityName)) return true
-
-        String groupName = efi.getEntityGroupName(ed.entityName)
-        Connection con = null
-        ResultSet tableSet = null
-        try {
-            con = efi.getConnection(groupName)
-            DatabaseMetaData dbData = con.getMetaData()
-
-            String[] types = ["TABLE", "VIEW", "ALIAS", "SYNONYM"]
-            tableSet = dbData.getTables(null, ed.getSchemaName(), ed.getTableName(), types)
-            if (tableSet.next()) {
-                existingTableEntityNames.add(ed.entityName)
-                return true
-            } else {
-                logger.info("Table for entity [${ed.entityName}] does NOT exist")
-                return false
-            }
-        } catch (Exception e) {
-            throw new EntityException("Exception checking to see if table [${ed.getTableName()}] exists", e)
-        } finally {
-            if (tableSet != null) tableSet.close()
-            if (con != null) con.close()
         }
     }
 }
