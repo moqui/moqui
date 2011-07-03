77a55dd94c6533030e2f036429194cfcc893bc8e
David E. Jones <dejc@me.com>
2011-07-03 11:41:26 -0600

    Added ArtifactAuthorizationException; note that this is an out of order commit and a previous commit depends on it

---
+++ framework/api/src/org/moqui/context/ArtifactAuthorizationException.java
@@ -0,0 +1,28 @@
+/*
+ * This Work is in the public domain and is provided on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
+ * including, without limitation, any warranties or conditions of TITLE,
+ * NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
+ * You are solely responsible for determining the appropriateness of using
+ * this Work and assume any risks associated with your use of this Work.
+ *
+ * This Work includes contributions authored by David E. Jones, not as a
+ * "work for hire", who hereby disclaims any copyright to the same.
+ */
+package org.moqui.context;
+
+import org.moqui.BaseException;
+
+/**
+ * TransactionException
+ */
+public class ArtifactAuthorizationException extends BaseException {
+
+    public ArtifactAuthorizationException(String str) {
+        super(str);
+    }
+
+    public ArtifactAuthorizationException(String str, Throwable nested) {
+        super(str, nested);
+    }
+}
