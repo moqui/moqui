moqui/moqui

commit 4a455fd48b4c3b275a7e84464478ae305a6b47ac

Entity find on a view-entity now automatically determines which   member-entities to join in based on the fields selected, in order by, and   in conditions; with this in place the need for DynamicViewEntity is   minimal because you can just select the relevant fields and not worry   about tables being joined in to the query that are not needed; this makes   it much easier to write queries that will run well on large databases; also some more testing and bug fixes; also some small API improvements to make finds easier

framework/src/main/groovy/org/moqui/impl/service/ServiceDefinition.groovy
@@ -248,18 +248,17 @@ class ServiceDefinition {
                 if (parameterNode."@required" == "true") {
                     eci.message.addValidationError(null, "${namePrefix}${parameterName}", "Field cannot be empty (service ${getServiceName()})", null)
                 }
-                // if it isn't there continue since there is nothing to do with it
                 // NOTE: should we change empty values to null? for now, no
-                continue
+                // if it isn't there continue on since since default-value, etc are handled below
             }
 
             // check type
             Object converted = checkConvertType(parameterNode, namePrefix, parameterName, parameterValue, rootParameters, eci)
             if (converted != null) {
                 parameterValue = converted
-            } else {
+            } else if (parameterValue) {
                 // no type conversion? error time...
-                eci.message.addValidationError(null, "${namePrefix}${parameterName}", "Field was type [${parameterValue.class.name}], expecting type [${type}] (service ${getServiceName()})", null)
+                eci.message.addValidationError(null, "${namePrefix}${parameterName}", "Field was type [${parameterValue?.class?.name}], expecting type [${type}] (service ${getServiceName()})", null)
                 continue
             }
 
@@ -387,10 +386,13 @@ class ServiceDefinition {
         // set the default-value if applicable
         if (!parameterValue && !(parameterValue instanceof Boolean) && parameterNode."@default-value") {
             ((ContextStack) eci.context).push(rootParameters)
-            parameterValue = eci.getResource().evaluateStringExpand(parameterNode."@default-value", "${this.location}_${parameterName}_default")
-            // logger.warn("TOREMOVE For parameter ${namePrefix}${parameterName} new value ${parameterValue} from default-value ${parameterNode.'@default-value'} and context: ${eci.context}")
+            parameterValue = eci.getResource().evaluateContextField(parameterNode."@default-value", "${this.location}_${parameterName}_default")
+            // logger.warn("For parameter ${namePrefix}${parameterName} new value ${parameterValue} from default-value [${parameterNode.'@default-value'}] and context: ${eci.context}")
             ((ContextStack) eci.context).pop()
         }
+
+        // if no default, don't try to convert
+        if (!parameterValue) return null
 
         String type = parameterNode."@type" ?: "String"
         if (!StupidUtilities.isInstanceOf(parameterValue, type)) {

framework/src/api/java/org/moqui/entity/EntityFind.java
@@ -152,12 +152,13 @@ public interface EntityFind extends java.io.Serializable {
     EntityFind distinct(boolean distinct);
     boolean getDistinct();
 
-    /** The limit, ie max number of rows to return. Default (null) means all rows.
+    /** The offset, ie the starting row to return. Default (null) means start from the first actual row.
      * Only applicable for list() and iterator() finds.
      *
      * @return Returns this for chaining of method calls.
      */
     EntityFind offset(Integer offset);
+    EntityFind offset(int pageIndex, int pageSize);
     Integer getOffset();
 
     /** The limit, ie max number of rows to return. Default (null) means all rows.

framework/src/main/groovy/org/moqui/impl/entity/EntityFindBase.groovy
@@ -218,13 +218,7 @@ abstract class EntityFindBase implements EntityFind {
 
         // always look for an orderByField parameter too
         String orderByString = inf.get("orderByField") ?: defaultOrderBy
-        if (orderByString) {
-            if (orderByString.contains(",")) {
-                for (String obsPart in orderByString.split(",")) this.orderBy(obsPart.trim())
-            } else {
-                this.orderBy(orderByString)
-            }
-        }
+        this.orderBy(orderByString)
 
         // look for the pageIndex and optional pageSize parameters
         if (alwaysPaginate || inf.get("pageIndex")) {
@@ -308,7 +302,13 @@ abstract class EntityFindBase implements EntityFind {
     /** @see org.moqui.entity.EntityFind#orderBy(String) */
     EntityFind orderBy(String orderByFieldName) {
         if (!this.orderByFields) this.orderByFields = new ArrayList()
-        if (orderByFieldName) this.orderByFields.add(orderByFieldName)
+        if (orderByFieldName) {
+            if (orderByFieldName.contains(",")) {
+                for (String obsPart in orderByFieldName.split(",")) this.orderByFields.add(obsPart.trim())
+            } else {
+                this.orderByFields.add(orderByFieldName)
+            }
+        }
         return this
     }
 
@@ -337,6 +337,8 @@ abstract class EntityFindBase implements EntityFind {
 
     /** @see org.moqui.entity.EntityFind#offset(int) */
     EntityFind offset(Integer offset) { this.offset = offset; return this }
+    /** @see org.moqui.entity.EntityFind#offset(int, int) */
+    EntityFind offset(int pageIndex, int pageSize) { offset(pageIndex * pageSize) }
     /** @see org.moqui.entity.EntityFind#getOffset() */
     Integer getOffset() { return this.offset }
 

framework/src/main/groovy/org/moqui/impl/entity/EntityFindBuilder.groovy
@@ -14,6 +14,7 @@ package org.moqui.impl.entity
 import java.sql.PreparedStatement
 import java.sql.SQLException
 import org.moqui.impl.entity.condition.EntityConditionImplBase
+import org.moqui.entity.EntityException
 
 class EntityFindBuilder extends EntityQueryBuilder {
     protected final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(EntityFindBuilder.class)
@@ -106,6 +107,23 @@ class EntityFindBuilder extends EntityQueryBuilder {
         }
     }
 
+    void expandJoinFromAlias(Node entityNode, String searchEntityAlias, Set<String> entityAliasUsedSet, Set<String> entityAliasesJoinedInSet) {
+        // first see if it needs expanding
+        if (entityAliasesJoinedInSet.contains(searchEntityAlias)) return
+        // find the a link back one in the set
+        Node memberEntityNode = (Node) entityNode."member-entity".find({ it."@entity-alias" == searchEntityAlias })
+        if (memberEntityNode == null)
+            throw new EntityException("Could not find member-entity with entity-alias [${searchEntityAlias}] in entity [${entityNode."@entity-name"}]")
+        String joinFromAlias = memberEntityNode."@join-from-alias"
+        if (entityAliasesJoinedInSet.contains(joinFromAlias)) {
+            entityAliasesJoinedInSet.add(searchEntityAlias)
+            entityAliasUsedSet.add(joinFromAlias)
+        } else {
+            // recurse to find member-entity with joinFromAlias, add in its joinFromAlias until one is found that is already in the set
+            expandJoinFromAlias(entityNode, joinFromAlias, entityAliasUsedSet, entityAliasesJoinedInSet)
+        }
+    }
+
     void makeSqlFromClause() {
         makeSqlFromClause(this.mainEntityDefinition, this.sqlTopLevel)
     }
@@ -124,6 +142,33 @@ class EntityFindBuilder extends EntityQueryBuilder {
 
             boolean useParenthesis = ("ansi" == joinStyle)
 
+            // get a list of all aliased fields selected or ordered by and don't bother joining in a member-entity
+            //     that is not selected or ordered by
+            Set<String> entityAliasUsedSet = new HashSet<String>()
+            Set<String> fieldUsedSet = new HashSet<String>()
+            EntityConditionImplBase viewWhere = localEntityDefinition.makeViewWhereCondition()
+            if (viewWhere != null) viewWhere.getAllAliases(entityAliasUsedSet, fieldUsedSet)
+            if (entityFindBase.whereEntityCondition != null)
+                entityFindBase.whereEntityCondition.getAllAliases(entityAliasUsedSet, fieldUsedSet)
+            if (entityFindBase.havingEntityCondition != null)
+                entityFindBase.havingEntityCondition.getAllAliases(entityAliasUsedSet, fieldUsedSet)
+            fieldUsedSet.addAll(entityFindBase.fieldsToSelect)
+            if (entityFindBase.orderByFields) fieldUsedSet.addAll(entityFindBase.orderByFields)
+            for (String fieldName in fieldUsedSet) {
+                Node aliasNode = localEntityDefinition.getFieldNode(fieldName)
+                if (aliasNode?."@entity-alias") entityAliasUsedSet.add(aliasNode."@entity-alias")
+            }
+
+            // make sure each entityAlias in the entityAliasUsedSet links back to the main
+            String mainEntityAlias = entityNode."member-entity".find({ !it."@join-from-alias" })?."@entity-alias"
+            Set<String> entityAliasesJoinedInSet = new HashSet<String>()
+            entityAliasesJoinedInSet.add(mainEntityAlias)
+            for (String entityAlias in entityAliasUsedSet) {
+                expandJoinFromAlias(entityNode, entityAlias, entityAliasUsedSet, entityAliasesJoinedInSet)
+            }
+
+            // logger.warn("entityAliasUsedSet=${entityAliasUsedSet} for entity ${localEntityDefinition.entityName}, fieldsToSelect=${entityFindBase.fieldsToSelect}, orderByFields=${entityFindBase.orderByFields}")
+
             // keep a set of all aliases in the join so far and if the left entity alias isn't there yet, and this
             // isn't the first one, throw an exception
             Set<String> joinedAliasSet = new TreeSet<String>()
@@ -132,7 +177,10 @@ class EntityFindBuilder extends EntityQueryBuilder {
             boolean isFirst = true
             boolean fromEmpty = true
             for (Node relatedMemberEntity in entityNode."member-entity") {
+                // if this isn't joined in skip it (should be first one only); the first is handled below
                 if (!relatedMemberEntity."@join-from-alias") continue
+                // if entity alias not used don't join it in
+                if (!entityAliasUsedSet.contains(relatedMemberEntity."@entity-alias")) continue
 
                 if (isFirst && useParenthesis) localBuilder.append('(')
 
@@ -215,13 +263,16 @@ class EntityFindBuilder extends EntityQueryBuilder {
 
             // handle member-entities not referenced in any view-link element
             for (Node memberEntity in entityNode."member-entity") {
+                // if entity alias not used don't join it in
+                if (!entityAliasUsedSet.contains(memberEntity."@entity-alias")) continue
+
+                if (joinedAliasSet.contains(memberEntity."@entity-alias")) continue
+
                 EntityDefinition fromEntityDefinition = this.efi.getEntityDefinition(memberEntity."@entity-name")
-                if (!joinedAliasSet.contains(memberEntity."@entity-alias")) {
-                    if (fromEmpty) fromEmpty = false else localBuilder.append(", ")
-                    makeSqlViewTableName(fromEntityDefinition, localBuilder)
-                    localBuilder.append(" ")
-                    localBuilder.append(memberEntity."@entity-alias")
-                }
+                if (fromEmpty) fromEmpty = false else localBuilder.append(", ")
+                makeSqlViewTableName(fromEntityDefinition, localBuilder)
+                localBuilder.append(" ")
+                localBuilder.append(memberEntity."@entity-alias")
             }
         } else {
             localBuilder.append(localEntityDefinition.getFullTableName())
@@ -366,6 +417,7 @@ class EntityFindBuilder extends EntityQueryBuilder {
     PreparedStatement makePreparedStatement() {
         if (!this.connection) throw new IllegalStateException("Cannot make PreparedStatement, no Connection in place")
         String sql = this.getSqlTopLevel().toString()
+        // logger.warn("making PreparedStatement for SQL: ${sql}")
         try {
             this.ps = connection.prepareStatement(sql, this.entityFindBase.resultSetType, this.entityFindBase.resultSetConcurrency)
             if (this.entityFindBase.maxRows > 0) this.ps.setMaxRows(this.entityFindBase.maxRows)

framework/src/main/groovy/org/moqui/impl/entity/condition/BasicJoinCondition.groovy
@@ -52,6 +52,11 @@ class BasicJoinCondition extends EntityConditionImplBase {
         return this.rhs.mapMatches(map)
     }
 
+    void getAllAliases(Set<String> entityAliasSet, Set<String> fieldAliasSet) {
+        lhs.getAllAliases(entityAliasSet, fieldAliasSet)
+        rhs.getAllAliases(entityAliasSet, fieldAliasSet)
+    }
+
     @Override
     EntityCondition ignoreCase() { throw new IllegalArgumentException("Ignore case not supported for this type of condition.") }
 

framework/src/main/groovy/org/moqui/impl/entity/condition/DateCondition.groovy
@@ -34,6 +34,11 @@ class DateCondition extends EntityConditionImplBase {
         this.makeCondition().makeSqlWhere(eqb)
     }
 
+    void getAllAliases(Set<String> entityAliasSet, Set<String> fieldAliasSet) {
+        fieldAliasSet.add(fromFieldName)
+        fieldAliasSet.add(thruFieldName)
+    }
+
     @Override
     boolean mapMatches(Map<String, ?> map) {
         return this.makeCondition().mapMatches(map)

framework/src/main/groovy/org/moqui/impl/entity/condition/EntityConditionImplBase.groovy
@@ -24,4 +24,6 @@ abstract class EntityConditionImplBase implements EntityCondition {
 
     /** Build SQL Where text to evaluate condition in a database. */
     public abstract void makeSqlWhere(EntityQueryBuilder eqb)
+
+    public abstract void getAllAliases(Set<String> entityAliasSet, Set<String> fieldAliasSet)
 }

framework/src/main/groovy/org/moqui/impl/entity/condition/FieldToFieldCondition.groovy
@@ -51,6 +51,20 @@ class FieldToFieldCondition extends EntityConditionImplBase {
         return compareByOperator(map.get(field.getFieldName()), this.operator, map.get(toField.getFieldName()))
     }
 
+    void getAllAliases(Set<String> entityAliasSet, Set<String> fieldAliasSet) {
+        // this will only be called for view-entity, so we'll either have a entityAlias or an aliased fieldName
+        if (field.entityAlias) {
+            entityAliasSet.add(field.entityAlias)
+        } else {
+            fieldAliasSet.add(field.fieldName)
+        }
+        if (toField.entityAlias) {
+            entityAliasSet.add(toField.entityAlias)
+        } else {
+            fieldAliasSet.add(toField.fieldName)
+        }
+    }
+
     @Override
     EntityCondition ignoreCase() { this.ignoreCase = true; return this }
 

commit 36383a81b63e7ef611fc66269b5112774865eabe

Fixed bug introduced in commit 4a455fd48b4c3b275a7e84464478ae305a6b47ac so that service parameter.@default-value actually works; previous change to evaluateContextField was not necessary to fix a previous issue along with other changes, and introduced problems with string literals

framework/src/main/groovy/org/moqui/impl/service/ServiceDefinition.groovy
@@ -386,7 +386,7 @@ class ServiceDefinition {
         // set the default-value if applicable
         if (!parameterValue && !(parameterValue instanceof Boolean) && parameterNode."@default-value") {
             ((ContextStack) eci.context).push(rootParameters)
-            parameterValue = eci.getResource().evaluateContextField(parameterNode."@default-value", "${this.location}_${parameterName}_default")
+            parameterValue = eci.getResource().evaluateStringExpand(parameterNode."@default-value", "${this.location}_${parameterName}_default")
             // logger.warn("For parameter ${namePrefix}${parameterName} new value ${parameterValue} from default-value [${parameterNode.'@default-value'}] and context: ${eci.context}")
             ((ContextStack) eci.context).pop()
         }