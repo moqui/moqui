14150bfa9dc9ac766647ac218e5e6f3b94906551
David E Jones <dej@dejc.com>
2012-11-17 12:54:50 -0800

    Added tests for local, timeZone, currencyUomId, and has permission; added permission example data

--- framework/src/test/groovy/UserFacadeTests.groovy
+++ framework/src/test/groovy/UserFacadeTests.groovy
@@ -33,13 +33,44 @@ class UserFacadeTests extends Specification {
         ec.user.loginUser("john.doe", "moqui", null)
     }
 
-    def "check userId username currencyUomId locale userAccount.userFullName"() {
+    def "check userId username currencyUomId locale userAccount.userFullName defaults"() {
         expect:
         ec.user.userId == "EX_JOHN_DOE"
         ec.user.username == "john.doe"
-        ec.user.currencyUomId == "USD"
         ec.user.locale.toString() == "en_us"
+        ec.user.timeZone.ID == "US/Central"
+        ec.user.currencyUomId == "USD"
         ec.user.userAccount.userFullName == "John Doe"
+    }
+
+    def "set and get Locale"() {
+        expect:
+        ec.user.setLocale(Locale.UK)
+        // doesn't work because of Java API (results in "en_gb" == "en_GB) ec.user.getLocale() == Locale.UK
+        ec.user.getLocale().toString() == "en_gb"
+        // set back to en_us
+        ec.user.setLocale(Locale.US)
+        ec.user.locale.toString() == "en_us"
+    }
+
+    def "set and get TimeZone"() {
+        expect:
+        ec.user.setTimeZone(TimeZone.getTimeZone("US/Pacific"))
+        ec.user.getTimeZone() == TimeZone.getTimeZone("US/Pacific")
+        ec.user.getTimeZone().getID() == "US/Pacific"
+        ec.user.getTimeZone().getRawOffset() == -28800000
+        // set TimeZone back to default US/Central
+        ec.user.setTimeZone(TimeZone.getTimeZone("US/Central"))
+        ec.user.getTimeZone().getID() == "US/Central"
+    }
+
+    def "set and get currencyUomId"() {
+        expect:
+        ec.user.setCurrencyUomId("GBP")
+        ec.user.getCurrencyUomId() == "GBP"
+        // reset to the default USD
+        ec.user.setCurrencyUomId("USD")
+        ec.user.getCurrencyUomId() == "USD"
     }
 
     def "check userGroupIdSet and isInGroup for ALL_USERS and ADMIN"() {
@@ -48,6 +79,12 @@ class UserFacadeTests extends Specification {
         ec.user.isInGroup("ALL_USERS")
         ec.user.userGroupIdSet.contains("ADMIN")
         ec.user.isInGroup("ADMIN")
+    }
+
+    def "check default admin group permission ExamplePerm"() {
+        expect:
+        ec.user.hasPermission("ExamplePerm")
+        !ec.user.hasPermission("BogusPerm")
     }
 
     def "not in web context so no visit"() {
