8cb7fcfd8df463c8e23692522d39bad534d47cee
David E Jones <dej@dejc.com>
2013-10-01 17:43:04 -0700

    Fixed issue with null oldStatusId and setting new statusChanged automatic   fields in entity-auto service implementation

--- framework/src/main/groovy/org/moqui/impl/service/runner/EntityAutoServiceRunner.groovy
+++ framework/src/main/groovy/org/moqui/impl/service/runner/EntityAutoServiceRunner.groovy
@@ -146,11 +146,16 @@ public class EntityAutoServiceRunner implements ServiceRunner {
 
         // populate the oldStatusId out if there is a service parameter for it, and before we do the set non-pk fields
         Node statusIdField = ed.getFieldNode("statusId")
-        if ((outParamNames == null || outParamNames.contains("oldStatusId")) && statusIdField) {
+        if (statusIdField != null) {
+            // do the actual query so we'll have the current statusId
+            lookedUpValue = sfi.getEcfi().getEntityFacade().makeFind(ed.getFullEntityName()).condition(parameters).useCache(false).forUpdate(true).one()
+        }
+        if (statusIdField != null && (outParamNames == null || outParamNames.contains("oldStatusId"))) {
             result.put("oldStatusId", lookedUpValue.get("statusId"))
         }
-        if ((outParamNames == null || outParamNames.contains("statusChanged")) && statusIdField) {
+        if (statusIdField != null && (outParamNames == null || outParamNames.contains("statusChanged"))) {
             result.put("statusChanged", !(lookedUpValue.get("statusId") == parameters.get("statusId")))
+            // logger.warn("========= oldStatusId=${result.oldStatusId}, statusChanged=${result.statusChanged}, lookedUpValue.statusId=${lookedUpValue.statusId}, parameters.statusId=${parameters.statusId}, lookedUpValue=${lookedUpValue}")
         }
 
         // do the StatusValidChange check
