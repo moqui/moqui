5be5ed34753d3c2599cace8dfeb4f714c95dfae2
David E Jones <dej@dejc.com>
2014-05-24 16:16:51 -0700

    Fixed another infinite recusion on init error, this time with Tenant entity facade isntances

--- framework/src/main/groovy/org/moqui/impl/context/ExecutionContextFactoryImpl.groovy
+++ framework/src/main/groovy/org/moqui/impl/context/ExecutionContextFactoryImpl.groovy
@@ -760,7 +760,8 @@ class ExecutionContextFactoryImpl implements ExecutionContextFactory {
     }
 
     protected Set<String> entitiesToSkipHitCount = new TreeSet(['moqui.server.ArtifactHit', 'moqui.server.ArtifactHitBin',
-            'moqui.entity.SequenceValueItem', 'moqui.security.UserAccount'])
+            'moqui.entity.SequenceValueItem', 'moqui.security.UserAccount', 'moqui.tenant.Tenant',
+            'moqui.tenant.TenantDataSource', 'moqui.tenant.TenantDataSourceXaProp'])
     void countArtifactHit(String artifactType, String artifactSubType, String artifactName, Map parameters,
                           long startTime, long endTime, Long outputSize) {
         // don't count the ones this calls
