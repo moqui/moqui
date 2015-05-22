f56f339ed890264d62cae33e8765a758178aeddf
David E Jones <dej@dejc.com>
2015-05-22 14:01:02 -0700

    Updated headers for CC0, better terms and more clear definition

--- framework/src/api/java/org/moqui/Moqui.java
+++ framework/src/api/java/org/moqui/Moqui.java
@@ -1,7 +1,14 @@
 /*
- * This is free and unencumbered software released into the public domain.
- * For specific language governing permissions and limitations refer to
- * the LICENSE.md file or http://unlicense.org
+ * This software is in the public domain under CC0 1.0 Universal.
+ * 
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to the
+ * public domain worldwide. This software is distributed without any
+ * warranty.
+ * 
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software (see the LICENSE.md file). If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 package org.moqui;
 
