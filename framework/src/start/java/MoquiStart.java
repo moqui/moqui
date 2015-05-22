7cd97392d39d60bd39eaa60e457cd86fcf40001a
David E Jones <dej@dejc.com>
2015-05-22 12:44:07 -0700

    Simplified license header, refer to unlicense explicitly

--- framework/src/start/java/MoquiStart.java
+++ framework/src/start/java/MoquiStart.java
@@ -1,13 +1,7 @@
 /*
- * This Work is in the public domain and is provided on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied,
- * including, without limitation, any warranties or conditions of TITLE,
- * NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
- * You are solely responsible for determining the appropriateness of using
- * this Work and assume any risks associated with your use of this Work.
- *
- * This Work includes contributions authored by David E. Jones, not as a
- * "work for hire", who hereby disclaims any copyright to the same.
+ * This is free and unencumbered software released into the public domain.
+ * For specific language governing permissions and limitations refer to
+ * the LICENSE.md file or http://unlicense.org
  */
 
 import java.io.*;
