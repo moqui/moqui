6bf8810dad4de2edb9af1e2bc7d69b874e2347c2
David E Jones <dej@dejc.com>
2015-10-26 22:35:48 -0700

    Use UTF-8 for the character set reading FTL files, as per feedback from Yao Chunlin

--- framework/src/main/groovy/org/moqui/impl/context/renderer/FtlTemplateRenderer.groovy
+++ framework/src/main/groovy/org/moqui/impl/context/renderer/FtlTemplateRenderer.groovy
@@ -75,7 +75,7 @@ class FtlTemplateRenderer implements TemplateRenderer {
         Template newTemplate = null
         Reader templateReader = null
         try {
-            templateReader = new InputStreamReader(ecfi.resourceFacade.getLocationStream(location))
+            templateReader = new InputStreamReader(ecfi.resourceFacade.getLocationStream(location), "UTF-8")
             newTemplate = new Template(location, templateReader, getFtlConfiguration())
         } catch (Exception e) {
             throw new IllegalArgumentException("Error while initializing template at [${location}]", e)
