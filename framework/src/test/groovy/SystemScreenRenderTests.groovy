87d8d7525f8527f115aad2de8911e2f904b21486
David E Jones <dej@dejc.com>
2015-11-17 09:37:27 -0800

    Log screen test stats

--- framework/src/test/groovy/SystemScreenRenderTests.groovy
+++ framework/src/test/groovy/SystemScreenRenderTests.groovy
@@ -37,6 +37,9 @@ class SystemScreenRenderTests extends Specification {
     }
 
     def cleanupSpec() {
+        long totalTime = System.currentTimeMillis() - screenTest.startTime
+        logger.info("Rendered ${screenTest.renderCount} screens (${screenTest.errorCount} errors) in ${ec.l10n.format(totalTime/1000, "0.000")}s, output ${ec.l10n.format(screenTest.renderTotalChars/1000, "#,##0")}k chars")
+
         ec.destroy()
     }
 
