a1f7a37546a8af89032b8343ee106201b3787aad
David E Jones <dej@dejc.com>
2015-11-18 12:30:05 -0800

    Moved DataView screens to separate test case, code more clear

--- framework/src/test/groovy/ToolsScreenRenderTests.groovy
+++ framework/src/test/groovy/ToolsScreenRenderTests.groovy
@@ -65,6 +65,7 @@ class ToolsScreenRenderTests extends Specification {
         screenPath | containsText1 | containsText2
         "dashboard" | "" | ""
 
+        // AutoScreen screens
         "AutoScreen/MainEntityList" | "" | ""
         "AutoScreen/AutoFind?aen=example.Example" | "Test Example Name" | "In Design [EXST_IN_DESIGN]"
         "AutoScreen/AutoEdit/AutoEditMaster?exampleId=TEST1&aen=example.Example" | "Test Example Name" | "example1@test.com"
@@ -73,21 +74,20 @@ class ToolsScreenRenderTests extends Specification {
         "AutoScreen/AutoFind/create?aen=example.Example&exampleId=TEST_SCR&exampleName=Screen Test Example&exampleTypeEnumId=EXT_MADE_UP&statusId=EXST_IN_DESIGN" | "" | ""
         "AutoScreen/AutoEdit/AutoEditMaster?exampleId=TEST_SCR&aen=example.Example" | "Screen Test Example" | ""
 
+        // ArtifactStats screen
         // don't run, takes too long: "ArtifactStats" | "" | ""
 
-        // create a DbViewEntity, set MASTER and fields, view it
-        "DataView/FindDbView/create?dbViewEntityName=ExampleDbView&packageName=example&isDataView=Y" | "" | ""
-        "DataView/FindDbView" | "ExampleDbView" | ""
-        "DataView/EditDbView/setMasterEntity?dbViewEntityName=ExampleDbView&entityAlias=MASTER&entityName=example.Example" | "" | ""
-        "DataView/EditDbView/setMasterFields?dbViewEntityName_0=ExampleDbView&field_0=example.Example.exampleName&dbViewEntityName_1=ExampleDbView&field_1=Example#moqui.basic.StatusItem.description&dbViewEntityName_2=ExampleDbView&field_2=ExampleType#moqui.basic.Enumeration.description" | "" | ""
-        "DataView/ViewDbView?dbViewEntityName=ExampleDbView" | "Screen Test Example" | "In Design"
+        // DataView screens
+        // see "render DataView screens"
 
+        // Entity/DataEdit screens
         "Entity/DataEdit/EntityList" | "ExampleContent" | "example"
         "Entity/DataEdit/EntityDetail?entityName=example.Example" | "text-medium" | "moqui.basic.Enumeration"
         "Entity/DataEdit/EntityDataFind?entityName=example.Example" | "Screen Test Example" | "In Design"
         "Entity/DataEdit/EntityDataEdit?exampleId=TEST1&entityName=example.Example" | "Test description, with a comma" | "example1@test.com"
         "Entity/DataEdit/EntityDataFind?exampleId=TEST1&entityName=example.ExampleItem" | "Test 1 Item 1" | "exampleItemSeqId"
 
+        // Other Entity screens
         "Entity/DataExport" | "example.Example" | ""
         // TODO: support response object with Writer in ScreenTestImpl/WebFacadeStub
         // "Entity/DataExport/EntityExport?entityNames=example.Example&dependentLevels=1&fileType=JSON&output=browser" | "Test Example Name" | "exampleItemSeqId"
@@ -96,11 +96,46 @@ class ToolsScreenRenderTests extends Specification {
         // run with very few baseCalls so it doesn't take too long
         "Entity/SpeedTest?baseCalls=10" | "" | ""
 
+        // Service screens
         "Service/ServiceReference?serviceName=example" | "org.moqui.example.ExampleServices.create#ExampleItem" | "Service Detail"
         "Service/ServiceDetail?serviceName=org.moqui.example.ExampleServices.consume#ExampleMessage" | "moqui.service.message.SystemMessage" | """ec.service.sync().name("store#example.Example")"""
         "Service/ServiceRun?serviceName=org.moqui.example.ExampleServices.create#ExampleItem" | "Example ID" | "Cron String"
         // run the service, then make sure it ran
         "Service/ServiceRun/run?serviceName=org.moqui.example.ExampleServices.create#ExampleItem&exampleId=TEST_SCR&description=ServiceRun Screen Test Item" | "" | ""
         "Entity/DataEdit/EntityDataFind?exampleId=TEST_SCR&entityName=example.ExampleItem" | "ServiceRun Screen Test Item" | ""
+    }
+
+    def "render DataView screens"() {
+        // create a DbViewEntity, set MASTER and fields, view it
+        when:
+        ScreenTestRender createStr = screenTest.render("DataView/FindDbView/create",
+                [dbViewEntityName:'ExampleDbView', packageName:'example', isDataView:'Y'], null)
+        logger.info("Called FindDbView/create in ${createStr.getRenderTime()}ms")
+
+        ScreenTestRender fdvStr = screenTest.render("DataView/FindDbView", null, null)
+        logger.info("Rendered DataView/FindDbView in ${fdvStr.getRenderTime()}ms, ${fdvStr.output?.length()} characters")
+
+        ScreenTestRender setMeStr = screenTest.render("DataView/EditDbView/setMasterEntity",
+                [dbViewEntityName:'ExampleDbView', entityAlias:'MASTER', entityName:'example.Example'], null)
+        logger.info("Called EditDbView/setMasterEntity in ${setMeStr.getRenderTime()}ms")
+
+        ScreenTestRender setMfStr = screenTest.render("DataView/EditDbView/setMasterFields",
+                [dbViewEntityName_0:'ExampleDbView', field_0:'example.Example.exampleName',
+                 dbViewEntityName_1:'ExampleDbView', field_1:'Example#moqui.basic.StatusItem.description',
+                 dbViewEntityName_2:'ExampleDbView', field_2:'ExampleType#moqui.basic.Enumeration.description'], null)
+        logger.info("Called EditDbView/setMasterFields in ${setMfStr.getRenderTime()}ms")
+
+        ScreenTestRender vdvStr = screenTest.render("DataView/ViewDbView?dbViewEntityName=ExampleDbView", null, null)
+        logger.info("Rendered DataView/FindDbView in ${vdvStr.getRenderTime()}ms, ${vdvStr.output?.length()} characters")
+
+        then:
+        !createStr.errorMessages
+        !fdvStr.errorMessages
+        fdvStr.assertContains("ExampleDbView")
+        !setMeStr.errorMessages
+        !setMfStr.errorMessages
+        !vdvStr.errorMessages
+        vdvStr.assertContains("Screen Test Example")
+        vdvStr.assertContains("In Design")
     }
 }
