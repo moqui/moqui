8b58728ccaf4f2d76b9bb9780f088cbd97902730
David E Jones <dej@dejc.com>
2015-11-22 12:07:12 -0800

    Comment formatting

--- framework/src/main/groovy/org/moqui/impl/context/TransactionInternalBitronix.groovy
+++ framework/src/main/groovy/org/moqui/impl/context/TransactionInternalBitronix.groovy
@@ -1,5 +1,6 @@
 /*
- * This software is in the public domain under CC0 1.0 Universal plus a Grant of Patent License.
+ * This software is in the public domain under CC0 1.0 Universal plus a 
+ * Grant of Patent License.
  * 
  * To the extent possible under law, the author(s) have dedicated all
  * copyright and related and neighboring rights to this software to the
