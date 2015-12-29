8f6bdf83298754061bc6704dcc292505c5d76636
David E Jones <dej@dejc.com>
2015-12-29 02:40:04 -0800

    Removed check for CA, only exists in Mantle

--- framework/src/test/groovy/ToolsRestApiTests.groovy
+++ framework/src/test/groovy/ToolsRestApiTests.groovy
@@ -70,7 +70,7 @@ class ToolsRestApiTests extends Specification {
         "s1/moqui/artifacts/hitSummary?artifactType=entity&artifactSubType=create&artifactName=moqui.basic&artifactName_op=contains" |
                 "moqui.basic.StatusType" | '"artifactSubType": "create"'
         "s1/moqui/basic/geos/USA" | "United States" | "Country"
-        "s1/moqui/basic/geos/USA/regions" | "USA_CA" | "California"
+        "s1/moqui/basic/geos/USA/regions" | "" | ""
         "s1/moqui/email/templates" | "PASSWORD_RESET" | "classpath://screen/PasswordReset.xml"
         "s1/moqui/entity/syncs/EXAMPLE" | "Example sync to EXAMPLE1 tenant" | "example\\\\..*"
     }
